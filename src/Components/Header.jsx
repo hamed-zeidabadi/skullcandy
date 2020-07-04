32d18a5332265fff9d2d214da15d1480bada591f
hamed-zeidabadi <zeidabadi73@gmail.com>
2020-07-04 22:31:16 +0430

    add Icon and Logo By Hamed

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,20 +1,51 @@
 import React from "react";
+import logo from "../Images/skull.png";
+import { FaUserAlt, FaSearch, FaShoppingBasket } from "react-icons/fa";
+
 const Header = () => {
   return (
-
     <>
-      <header className='header'>
-
-        <div className='header_container'>
+      <header className="header">
+        <div className="header_container">
           {/* Add two container for left & right items */}
-          <div className='header_container_right'></div>
-          <div className='header_container_left'></div>
-
+          <div className="header_container_right">
+            <img
+              src={logo}
+              alt="logo skull"
+              className="header_container_right_logo"
+            />
+            <nav className="header_container_right_nav">
+              <ul>
+                <li>
+                  <a href="#">خانه</a>
+                </li>
+                <li>
+                  <a href="#">تماس با ما</a>
+                </li>
+                <li>
+                  <a href="#">قوانین</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="header_container_left">
+            <nav className="header_container_left_nav">
+              <ul>
+                <li>
+                  <FaUserAlt />
+                </li>
+                <li>
+                  <FaSearch />
+                </li>
+                <li>
+                  <FaShoppingBasket />
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
-        
       </header>
     </>
-
   );
 };
 
