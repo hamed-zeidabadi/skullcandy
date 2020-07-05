0ce241c49087575581e24bf1120d714b04a62986
amin-da <darabniya@yahoo.com>
2020-07-05 18:51:26 +0430

    start working on Footer in project

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <div className="footer">
-        <p>Footer Component</p>
-      </div>
+     <footer className='footer'>
+
+     </footer>
     </>
   );
 };
