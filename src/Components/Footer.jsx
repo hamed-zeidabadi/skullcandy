6f59f45c8cafb18f3fad9067b601b4ccdffa02f3
amin-da <darabniya@yahoo.com>
2020-07-05 20:01:35 +0430

    add icons in footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,38 @@
 import React from "react";
+import { FaGithubAlt, FaInstagram, FaTwitter ,FaYoutube } from "react-icons/fa";
+
 const Footer = () => {
   return (
     <>
      <footer className='footer'>
       <section className='footer_container'>
-        <div className='footer_container_sign_up'>... اول گوش کنید</div>
+
+        <div className='footer_container_sign_up'>اول گوش کنید ...</div>
+
         <div className='footer_container_info'> نقشه سایت</div>
-        <div className='footer_container_social_media_icons'> ما را دنبال</div>
+
+        <div className='footer_container_social_media'>
+
+          <h6>ما را دنبال کنید</h6>
+          
+          <nav className="footer_container_social_media_icons'">
+              <ul>
+                <li>
+                  <FaGithubAlt />
+                </li>
+                <li>
+                  <FaInstagram />
+                </li>
+                <li>
+                  <FaTwitter />
+                </li> 
+                <li>
+                  <FaYoutube />
+                </li>
+              </ul>
+            </nav>
+
+        </div>
         
       </section>
      </footer>
