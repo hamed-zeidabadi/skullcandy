4eb8e2e6111feadd63ab05948bb859a5de711cef
hamed-zeidabadi <zeidabadi73@gmail.com>
2020-07-10 13:56:00 +0430

    create State and config for select default text

--- src/Components/Filter.jsx
+++ src/Components/Filter.jsx
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 const Filter = () => {
+  const [Slected, setSlected] = useState({
+    default: true,
+    msg: "",
+  });
   return (
     <>
       <div class="dropdown">
@@ -11,24 +15,51 @@ const Filter = () => {
         />
         <label for="filter-switch" class="dropdown__options-filter">
           <ul class="dropdown__filter" role="listbox" tabindex="-1">
-            <li class="dropdown__filter-selected" aria-selected="true">
-              جدیدترین ها
+            <li
+              class="dropdown__filter-selected"
+              style={{ color: "#4d1091" }}
+              aria-selected="true"
+            >
+              {Slected.default ? " نمایش بر اساس ...." : Slected.msg}
             </li>
             <li>
               <ul class="dropdown__select">
-                <li class="dropdown__select-option" role="option">
+                <li
+                  class="dropdown__select-option"
+                  role="option"
+                  onClick={() =>
+                    setSlected({ default: false, msg: "بیشترین فروش" })
+                  }
+                >
                   بیشترین فروش
                 </li>
-                <li class="dropdown__select-option" role="option">
-                  محبوترین
+                <li
+                  class="dropdown__select-option"
+                  role="option"
+                  onClick={() =>
+                    setSlected({ default: false, msg: "محبوترین ها" })
+                  }
+                >
+                  محبوترین ها
                 </li>
-                <li class="dropdown__select-option" role="option">
+                <li
+                  class="dropdown__select-option"
+                  role="option"
+                  onClick={() =>
+                    setSlected({ default: false, msg: "بیشترین قیمت" })
+                  }
+                >
                   بیشترین قیمت
                 </li>
-                <li class="dropdown__select-option" role="option">
+                <li
+                  class="dropdown__select-option"
+                  role="option"
+                  onClick={() =>
+                    setSlected({ default: false, msg: " کمترین قیمت" })
+                  }
+                >
                   کمترین قیمت
                 </li>
-               
               </ul>
             </li>
           </ul>
