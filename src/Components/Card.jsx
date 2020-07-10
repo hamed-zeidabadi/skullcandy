959429c666951170cfb6d93af6258aff57420afe
hamed-zeidabadi <zeidabadi73@gmail.com>
2020-07-10 21:56:50 +0430

    add filter by color

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,21 +1,37 @@
 import React, { useState, useEffect } from "react";
-import img from "../Images/T-Shirts/T-1.jpg";
+import red from "../Images/T-Shirts/T-1.jpg";
+import pink from "../Images/T-Shirts/T-7.jpg";
+import blue from "../Images/T-Shirts/T-8.jpg";
+import black from "../Images/T-Shirts/T-5.jpg";
 
 const Card = ({ title, image, color, price, like, buy }) => {
-  // const [Color, setColor] = useState({});
-  // useEffect(() => {
-  //   if (color) return setColor(color);
-  // }, []);
+  const [Color, setColor] = useState(null);
 
   return (
     <>
       <div className="card">
-        <img src={image} alt="product images" className="card_img" />
+        <img
+          src={Color ? Color : image}
+          alt="product images"
+          className="card_img"
+        />
         <div className="card_colors">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div
+            style={{ backgroundColor: "red" }}
+            onClick={() => setColor(red)}
+          ></div>
+          <div
+            style={{ backgroundColor: "blue" }}
+            onClick={() => setColor(blue)}
+          ></div>
+          <div
+            style={{ backgroundColor: "pink" }}
+            onClick={() => setColor(pink)}
+          ></div>
+          <div
+            style={{ backgroundColor: "black" }}
+            onClick={() => setColor(black)}
+          ></div>
         </div>
         <p className="card_desc_title"> تی شرت</p>
         <div className="card_desc">
